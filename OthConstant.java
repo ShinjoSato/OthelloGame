a684f60cc6d9cdd72048e3dd1a8f029fa278cf5c
ShinjoSato <34845177+ShinjoSato@users.noreply.github.com>
2019-11-01 06:54:51 +0900

    It works

--- OthConstant.java
+++ OthConstant.java
@@ -1,12 +1,14 @@
 import javax.swing.ImageIcon;
 
 interface OthConstant {
-        int size = 8, xwidth = 400, ywidth = 400, xzero = 200, yzero = 44;
-        String[] Stone = { "images/image002.png", // Image of blackStone
-                        "images/image001.png", // Image of whiteStone
-                        "images/black02.png", "images/white02.png", "images/white03.png", 
-                        "images/white04.png" }, Backgroung = { "images/nightpd.jpg" }, // Image of backgroung
-                        Board = { "images/board01.png" };
+        int size = 18, xwidth = 400, ywidth = 400, xzero = 200, yzero = 44;
+        String relative_path = "../images/";
+        String[] Stone = { relative_path + "image002.png", // Image of blackStone
+                        relative_path + "image001.png", // Image of whiteStone
+                        relative_path + "black02.png", relative_path + "white02.png", relative_path + "white03.png",
+                        relative_path + "white04.png" }, Backgroung = { relative_path + "nightpd.jpg" }, // Image of
+                                                                                                         // backgroung
+                        Board = { relative_path + "board01.png" };
         ImageIcon[] istone = { new ImageIcon(Stone[0]), new ImageIcon(Stone[1]), new ImageIcon(Stone[2]),
                         new ImageIcon(Stone[3]), new ImageIcon(Stone[4]), new ImageIcon(Stone[5]) },
                         backgroung = { new ImageIcon(Backgroung[0]) }, board = { new ImageIcon(Board[0]) };
