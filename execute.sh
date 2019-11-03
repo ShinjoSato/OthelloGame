61bcced6c4ff66970dc9ac232f48025b4616dbdb
ShinjoSato <34845177+ShinjoSato@users.noreply.github.com>
2019-11-04 08:29:32 +0900

    ディレクトリ構成の変更

--- execute.sh
+++ execute.sh
@@ -1,15 +1,7 @@
 cd src
 
-javac -d classes FrameBase.java
-javac -d classes HomePanel.java
-javac -d classes Main15.java
-javac -d classes OthConstant.java
-javac -d classes Othello.java
-javac -d classes OthelloArt.java
-javac -d classes OthPanel.java
-javac -d classes PlayPanel.java
-javac -d classes SetPanel.java
+javac -d ../bin FrameBase.java
 
-cd classes
+cd ../bin
 
 java FrameBase
